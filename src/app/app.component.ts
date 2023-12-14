9b50a42d0573393c588b8d82b2dab5aa3f7b57b4
Murat Karakaya <murty34@outlook.com>
2023-12-14 13:49:09 +0300

    repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
+//Data'yı yönettiğin yer
+//Component html'in datasını yönettiğimiz yer
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { CategoryComponent } from './components/category/category.component';
+import { HttpClient } from '@angular/common/http';
 
+//{} parantez obje demek
+// ./ aynı klasör demek
+// [] parantez array demek
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ProductComponent,
+    NaviComponent,
+    CategoryComponent,
+  ],
 })
 export class AppComponent {
-  title = 'northwind';
+  constructor(private httpClient: HttpClient) {}
+  title: string = 'northwind';
+  user = 'Engin Demiroğ';
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ProductResponseModel } from '../models/productResponseModel';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ProductService {
+  constructor(private httpClient: HttpClient) {}
+
+  apiUrl = 'https://localhost:44361/api/Products/getall';
+
+  getProducts(): Observable<ProductResponseModel> {
+    return this.httpClient.get<ProductResponseModel>(this.apiUrl);
+  }
+}
